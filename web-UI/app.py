e719a8aa1a6bbcab012fbad41f182b32aa3cbb21
ab5380 <ab5380@rit.edu>
2024-08-06 15:40:52 -0400

    assets

--- web-UI/app.py
+++ web-UI/app.py
@@ -16,7 +16,7 @@ def load_image(image_path):
 st.set_page_config(page_title=ui_title, page_icon=ui_icon, layout="wide")  # Set the page title, icon, and layout
 
 # Load the local image
-image_path = "/Users/aneeshbukya/Desktop/Projects/bart/web-UI/background-1920x1080.JPG"  # Update with the path to your local image
+image_path = "/Users/aneeshbukya/Brightcone.ai/bart/web-UI/background-1920x1080.jpg"  # Update with the path to your local image
 image_base64 = load_image(image_path)
 
 background_image = f"""
